51453657c94e74b454808281b6bed29a1014d5ac
Laszlo Kovari <laci@cloudera.com>
2021-02-22 22:33:24 +0100

    undo push to test

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"strings"
 )
 
 var originUnit string
@@ -18,10 +17,6 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
-	if len(os.Args) != 2 {
-		printError(errInvalidArguments)
-	}
-	originUnit = strings.ToUpper(os.Args[1])
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
